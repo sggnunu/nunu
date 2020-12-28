55fb6b00d03f54559c52ee2a8c3af5c5fba73c30
sggnunu <gorkem.gonen@gmail.com>
2020-12-28 12:12:17 +0300

    My Fifth Assignment

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -3,6 +3,8 @@
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
+var a = helloSpeaker;
+var b = byeSpeaker;
 
 for (var i = 0; i < names.length; i++) {
 
